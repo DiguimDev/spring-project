06617e799349d9b21a2093d3b5027a379fdf7673
Thauan <dev.thauan.rodrigues@gmail.com>
2024-09-25 21:16:58 -0300

    Payment add

---
+++ src/main/java/com/springproject/commerce/entities/Payment.java
@@ -0,0 +1,51 @@
+package com.springproject.commerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant Moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        Moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return Moment;
+    }
+
+    public void setMoment(Instant moment) {
+        Moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
